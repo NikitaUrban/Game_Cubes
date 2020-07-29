9a37890e42eb5c30b2fc7dc5c39e6b905256b63f
nikitka <nick.urbanovich@mail.ru>
2020-07-29 10:58:59 +0300

    add filer to repo

---
+++ script.js
@@ -0,0 +1,7 @@
+let btn1=document.querySelector('#btn1')
+let btn2=document.querySelector('#btn2')
+
+btn1.addEventListener('click',function () {
+    let buttons=document.querySelector('#buttons')
+    buttons.style.display='none'
+})
